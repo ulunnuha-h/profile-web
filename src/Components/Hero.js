f671b8f187a3b64ba0f7d27e6d54487856eb0ee9
Hanif Ulunnuha Hidayat <ulunnuha.h@gmail.com>
2023-02-12 23:07:22 +0700

    link hero button and fix heading size style

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -6,8 +6,8 @@ const Hero = () => {
             <span className="font-baskerville mb-5 text-gray-200 text-lg">Hello, my name is</span>
             <h1>Hanif Ulunnuha Hidayat</h1>
             <h1>A <span className="text-orange-400">front-end developer</span></h1>
-            <span className="font-baskerville text-gray-300">I ensure visitors comfortable when interact with the web page.</span>
-            <a href="#" className="btn-primary py-3 px-6 mt-16">See my projects</a>
+            <span className="font-baskerville text-gray-300 mx-9">I ensure visitors comfortable when interact with the web page.</span>
+            <a href="#project" className="btn-primary py-3 px-6 mt-16">See my projects</a>
         </div>
     )
 }
